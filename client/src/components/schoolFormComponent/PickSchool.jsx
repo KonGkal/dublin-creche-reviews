5731a73c5d229718b93035abbf0e7c80c599217a
KonGkal <konsgkal@gmail.com>
2021-06-12 15:27:47 +0100

    fix PickSchool component

--- client/src/components/schoolFormComponent/PickSchool.jsx
+++ client/src/components/schoolFormComponent/PickSchool.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-const PickSchool = () => {
-  return <div>Pick School</div>;
+const PickSchool = ({ schools }) => {
+  const [selectedSchool, setSelectedSchool] = useState([]);
+  console.log(selectedSchool);
+
+  const schoolList = schools.map((school) => (
+    <option value={school.id}>{school.name}</option>
+  ));
+
+  return (
+    <select
+      value={selectedSchool}
+      name="selectedSchool"
+      onChange={(e) => setSelectedSchool(e.target.value)}
+    >
+      {schoolList}
+    </select>
+  );
 };
 
 export default PickSchool;
