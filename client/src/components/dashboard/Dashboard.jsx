4de47ebc2f578c38bdf115315ebaa3cfb721e5c5
KonGkal <konsgkal@gmail.com>
2021-06-13 12:15:10 +0100

    migrate user details logic from ReviewForm to Dashboard

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import SchoolList from "../schoolsList/SchoolList";
 import ReviewForm from "../reviewForm/ReviewForm";
@@ -8,7 +8,44 @@ import MyReviews from "../myReviews/MyReviews";
 import "./Dashboard.css";
 import { ReactComponent as IconSvg } from "../../icons/noun_Child_1869302.svg";
 
+import { useAuth0 } from "@auth0/auth0-react";
+import { addNewUser, findUserbyEmail } from "../../services/apiService";
+
 const Dashboard = () => {
+  const [userDetails, setUserDetails] = useState([]);
+  const { user, getAccessTokenSilently } = useAuth0();
+  const { email } = user;
+
+  const getAllUsers = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const res = await fetch(`http://localhost:3001/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const isExistingUser = (userEmail) => {
+    getAllUsers().then((res) => {
+      if (res.filter((user) => user.email === userEmail).length === 0)
+        addNewUser(email);
+    });
+  };
+
+  isExistingUser(email);
+
+  useEffect(() => {
+    findUserbyEmail(email).then((user) => {
+      setUserDetails(user);
+    });
+  }, [email]);
+
   const [reviews, setReviews] = useState([]);
   return (
     <div className="dashboard-container">
@@ -16,7 +53,7 @@ const Dashboard = () => {
       <h1 className="school-list-header">Schools</h1>
       <Switch>
         <Route exact path="/" component={SchoolList} />
-        <ReviewsContext.Provider value={{ reviews, setReviews }}>
+        <ReviewsContext.Provider value={{ reviews, setReviews, userDetails }}>
           <Route path="/review" component={ReviewForm} />
           <Route path="/myReviews" component={MyReviews} />
           <Route
--- client/src/components/reviewForm/ReviewForm.jsx
+++ client/src/components/reviewForm/ReviewForm.jsx
@@ -1,6 +1,5 @@
-import { useState, useEffect, useContext } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { addNewUser, findUserbyEmail } from "../../services/apiService";
+import { useState, useContext } from "react";
+
 import "./ReviewForm.css";
 import SchoolFormContainer from "../schoolForm/SchoolFormContainer";
 import SelectedSchoolContext from "../../context/SelectedSchoolContext";
@@ -9,40 +8,8 @@ import { createReview } from "../../services/apiService";
 
 const ReviewForm = () => {
   const [selectedSchool, setSelectedSchool] = useState("");
-  const [userDetails, setUserDetails] = useState([]);
-  const { user, getAccessTokenSilently } = useAuth0();
   const { setReviews } = useContext(ReviewsContext);
-  const { email } = user;
-
-  const getAllUsers = async () => {
-    try {
-      const token = await getAccessTokenSilently();
-
-      const res = await fetch(`http://localhost:3001/users`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return res.json();
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const isExistingUser = (userEmail) => {
-    getAllUsers().then((res) => {
-      if (res.filter((user) => user.email === userEmail).length === 0)
-        addNewUser(email);
-    });
-  };
-
-  isExistingUser(email);
-
-  useEffect(() => {
-    findUserbyEmail(email).then((user) => {
-      setUserDetails(user);
-    });
-  }, [email]);
+  const { userDetails } = useContext(ReviewsContext);
 
   const [facility, setFacility] = useState("");
   const [staff, setStaff] = useState("");
