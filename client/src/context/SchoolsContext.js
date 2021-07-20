557e04e2afda6b4f261ba2d5ee40e4624e0ac7d0
KonGkal <konsgkal@gmail.com>
2021-07-20 15:52:57 +0300

    assign outdir

--- client/src/context/SchoolsContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var SchoolsContext = react_1.createContext(null);
-exports.default = SchoolsContext;
--- client/src/context/UserDetailsContext.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var UserDetailsContext = react_1.createContext(null);
-exports.default = UserDetailsContext;
