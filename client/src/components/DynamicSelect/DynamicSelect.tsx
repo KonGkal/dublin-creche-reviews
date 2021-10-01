c9ea17d380ff0982b05d6397bf89111c9bc8c4f7
KonGkal <konsgkal@gmail.com>
2021-10-01 21:33:07 +0100

    add: create selectOptions and return JSX

--- client/src/components/DynamicSelect/DynamicSelect.tsx
+++ client/src/components/DynamicSelect/DynamicSelect.tsx
@@ -1,3 +1,6 @@
+import { Dispatch, SetStateAction } from "react";
+import "../reviewForm/ReviewForm.css";
+
 type DynamicSelectProps = {
   range: number;
   startAt?: number;
@@ -21,6 +24,24 @@ const DynamicSelect = ({
   ): ReadonlyArray<number> {
     return [...Array(size).keys()].map((num) => num + startAt);
   }
+
+  const selectOptions = createArrayInRange(range, startAt).map((num) => {
+    return (
+      <option key={num} value={`${num}`}>
+        {num}
+      </option>
+    );
+  });
+
+  return (
+    <select
+      value={selectValue}
+      name={selectName}
+      onChange={(e) => set(e.target.value)}
+    >
+      {selectOptions}
+    </select>
+  );
 };
 
 export default DynamicSelect;
