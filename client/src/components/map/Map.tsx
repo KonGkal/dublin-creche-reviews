83ff7c7e961593aa5e2fa21c5a439c767e74ec5f
KonGkal <konsgkal@gmail.com>
2021-10-01 21:09:06 +0100

    add: create Map component

---
+++ client/src/components/map/Map.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react'
+
+const Map: FC = () => {
+
+
+  return (
+    
+  )
+
+}
+
+export default Map;
--- server/models/index.js
+++ server/models/index.js
@@ -10,10 +10,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   dialectOptions: {
-    ssl: { 
-      require: true,
-      rejectUnauthorized: false 
-    } 
+    // ssl: { 
+    //   require: true,
+    //   rejectUnauthorized: false 
+    // } 
   }
 });
 
