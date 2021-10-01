0717e7234011db49baeaddc907e3fb45ef747c16
KonGkal <konsgkal@gmail.com>
2021-10-01 21:29:50 +0100

    add: create DynamicSelectProps type

--- client/src/components/DynamicSelect/DynamicSelect.tsx
+++ client/src/components/DynamicSelect/DynamicSelect.tsx
@@ -1,3 +1,12 @@
+type DynamicSelectProps = {
+  range: number;
+  startAt?: number;
+  selectValue: string;
+  selectName: string;
+  header?: string;
+  set: Dispatch<SetStateAction<string>>;
+};
+
 const DynamicSelect = () => {};
 
 export default DynamicSelect;
